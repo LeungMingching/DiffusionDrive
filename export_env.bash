3733975c7b7de9901356726702c29230f9a5a8fe
Liang Mingjing <liangmingjing849@163.com>
2026-04-12 21:40:13 +0800

    export env vars

---
+++ export_env.bash
@@ -0,0 +1,5 @@
+export NUPLAN_MAP_VERSION="nuplan-maps-v1.0"
+export NUPLAN_MAPS_ROOT="$HOME/workspace/DiffusionDrive/dataset/maps"
+export NAVSIM_EXP_ROOT="$HOME/workspace/DiffusionDrive/exp"
+export NAVSIM_DEVKIT_ROOT="$HOME/workspace/DiffusionDrive/navsim"
+export OPENSCENE_DATA_ROOT="$HOME/workspace/DiffusionDrive/dataset"
